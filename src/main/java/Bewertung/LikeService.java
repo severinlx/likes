1f22c4c05f49e41e0961227263f04786d23a6780
geniusalina <38285023+geniusalina@users.noreply.github.com>
2019-02-21 14:42:44 +0100

    Update LikeService.java

--- src/main/java/Bewertung/LikeService.java
+++ src/main/java/Bewertung/LikeService.java
@@ -3,6 +3,7 @@ package Bewertung;
 //import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,6 +21,7 @@ import java.sql.*;
 public class LikeService {
 	//protected static final String[] POSTS = { "1" : {likes: anzahl, dislikes: anzahl}, "2", "3" };
 	PostEig postEig;
+	JsonObject result = new JsonObject();
 	//id, likes, dislikes
 	Integer[][] postFeaturesArray =  {{1,2,3}, {2,2,3}
 			};
@@ -33,13 +35,53 @@ public class LikeService {
 	{
 		
 		JsonObject result = new JsonObject();
-		//result.addProperty("post_id", Post_id);
-		//result.addProperty("likes", DBOperations.getLikes(Post_id));
-		//result.addProperty("dislikes", DBOperations.getDisLikes(Post_id));
-		result.addProperty("post_id", 9);
-		result.addProperty("likes", 10);
-		result.addProperty("dislikes",3);
+		result.addProperty("post_id", Post_id);
+		result.addProperty("likes", DBOperations.getLikes(Post_id));
+		result.addProperty("dislikes", DBOperations.getDisLikes(Post_id));
+		result.addProperty("user_id",0);
 		return result;
+		
+		
 	}
 	
-}
+	public String getPostId(JsonObject json) {
+		
+		return json.get("Post_id").getAsString();
+		
+
+		
+	}
+	@POST
+	@Path("/{new_like_nr}")
+	public void  incrementDislike(JsonObject json) {
+		
+		 DBOperations.incrementDisLike(getPostId(json));
+		
+	}
+	@POST
+	
+	public void  incrementlike(JsonObject json) {
+		
+		 DBOperations.incrementLike(getPostId(json));
+		
+	}
+	
+	//public JsonObject incrementBewertungNumber(int Post_id, String event) {
+		
+		
+	//	if(event=="like" )
+	//	{
+	//		result.addProperty("likes", DBOperations.getLikes(Post_id)+1);
+			
+	//	}		
+		
+	//	else if (event=="dislike") {
+			
+		//	result.addProperty("dislikes", DBOperations.getDisLikes(Post_id) + 1);
+	//	}
+		
+		
+	//	return result;
+	}
+	
+
