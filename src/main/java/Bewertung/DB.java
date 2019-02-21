0cf03cf724e15f907f180f41f12591f22563c4f5
Alexandru Severin <severinlx@yahoo.com>
2019-02-21 14:10:39 +0100

    refactor

--- src/main/java/Bewertung/DB.java
+++ src/main/java/Bewertung/DB.java
@@ -15,50 +15,63 @@ public class DB {
 	   static final String PASS = ""; 
 	  
 	   public static void createTable() { 
-	      Connection conn = null; 
-	      Statement stmt = null; 
-	      try { 
-	         // STEP 1: Register JDBC driver 
-	         Class.forName(JDBC_DRIVER); 
-	             
-	         //STEP 2: Open a connection 
-	         System.out.println("Connecting to database..."); 
-	         conn = DriverManager.getConnection(DB_URL,USER,PASS);  
-	         
-	         //STEP 3: Execute a query 
-	         System.out.println("Creating table in given database..."); 
-	         stmt = conn.createStatement(); 
-	         String sql =  "CREATE TABLE   Post_Rating " + 
-	        	" (post_id INTEGER, " +  
-	            " post_likes_number INTEGER, " +  
-	            " post_dislikes_number INTEGER, " +  
-	            " PRIMARY KEY ( post_id ))";  
-	         stmt.executeUpdate(sql);
-	         System.out.println("Created table in given database..."); 
-	         
-	         // STEP 4: Clean-up environment 
-	         stmt.close(); 
-	         conn.close(); 
-	      } catch(SQLException se) { 
-	         //Handle errors for JDBC 
-	         se.printStackTrace(); 
-	      } catch(Exception e) { 
-	         //Handle errors for Class.forName 
-	         e.printStackTrace(); 
-	      } finally { 
-	         //finally block used to close resources 
-	         try{ 
-	            if(stmt!=null) stmt.close(); 
-	         } catch(SQLException se2) { 
-	         } // nothing we can do 
-	         try { 
-	            if(conn!=null) conn.close(); 
-	         } catch(SQLException se){ 
-	            se.printStackTrace(); 
-	         } //end finally try 
-	      } //end try 
+		   String sql =  "CREATE TABLE   Post_Rating " + 
+		        	" (post_id INTEGER, " +  
+		            " post_likes_number INTEGER, " +  
+		            " post_dislikes_number INTEGER, " +  
+		            " PRIMARY KEY ( post_id ))";  
+		   
+	 
 	      System.out.println("Goodbye!");
-	   } 
+	      runQuery(sql);
+	   }
+	   
+	   public static void runQuery(String sql) {
+			  Connection conn = null; 
+		      Statement stmt = null; 
+		      try{
+		         // STEP 1: Register JDBC driver 
+			         Class.forName(JDBC_DRIVER);  
+		         
+		         // STEP 2: Open a connection 
+		         System.out.println("Connecting to a selected database..."); 
+		         conn = DriverManager.getConnection(DB_URL, USER, PASS); 
+		         System.out.println("Connected database successfully..."); 
+		         
+		         // STEP 3: Execute a query 
+		         stmt = conn.createStatement();  
+		         
+		         System.out.println(sql);
+		         
+		         stmt.executeUpdate(sql); 
+		         
+		         System.out.println("databased changed..."); 
+		         
+		         // STEP 4: Clean-up environment 
+		         stmt.close(); 
+		         conn.close(); 
+		      } catch(SQLException se) { 
+		         // Handle errors for JDBC 
+		         se.printStackTrace(); 
+		      } catch(Exception e) { 
+		         // Handle errors for Class.forName 
+		         e.printStackTrace(); 
+		      } finally { 
+		         // finally block used to close resources 
+		         try {
+		            if(stmt!=null) stmt.close();  
+		         } catch(SQLException se2) { 
+		         } // nothing we can do 
+		         try { 
+		            if(conn!=null) conn.close(); 
+		         } catch(SQLException se) { 
+		            se.printStackTrace(); 
+		         } // end finally try 
+		      } // end try 
+		      System.out.println("Goodbye!"); 
+			
+		}
+		
 	}
 
 
--- src/main/java/Bewertung/DBpost.java
+++ src/main/java/Bewertung/DBpost.java
@@ -18,61 +18,15 @@ public class DBpost {
 	        		 Integer.toString(postEig.getNr_of_likes()), 
 	        		 Integer.toString(postEig.getNr_of_dislikes())); 
 		   
-		   runQuery(sql);
+		   DB.runQuery(sql);
 	}
 	   
-	public static void runQuery(String sql) {
-		Connection conn = null; 
-	      Statement stmt = null; 
-	      try{
-	         // STEP 1: Register JDBC driver 
-		         Class.forName(JDBC_DRIVER);  
-	         
-	         // STEP 2: Open a connection 
-	         System.out.println("Connecting to a selected database..."); 
-	         conn = DriverManager.getConnection(DB_URL, USER, PASS); 
-	         System.out.println("Connected database successfully..."); 
-	         
-	         // STEP 3: Execute a query 
-	         stmt = conn.createStatement();  
-	         
-	         System.out.println(sql);
-	         
-	         stmt.executeUpdate(sql); 
-	         
-	         System.out.println("Inserted records into the table..."); 
-	         
-	         // STEP 4: Clean-up environment 
-	         stmt.close(); 
-	         conn.close(); 
-	      } catch(SQLException se) { 
-	         // Handle errors for JDBC 
-	         se.printStackTrace(); 
-	      } catch(Exception e) { 
-	         // Handle errors for Class.forName 
-	         e.printStackTrace(); 
-	      } finally { 
-	         // finally block used to close resources 
-	         try {
-	            if(stmt!=null) stmt.close();  
-	         } catch(SQLException se2) { 
-	         } // nothing we can do 
-	         try { 
-	            if(conn!=null) conn.close(); 
-	         } catch(SQLException se) { 
-	            se.printStackTrace(); 
-	         } // end finally try 
-	      } // end try 
-	      System.out.println("Goodbye!"); 
-		
-	}
-	
 	public static void update(PostEig postRating) {
 		String sql = String.format("UPDATE Post_Rating SET post_likes_number=%x, post_dislikes_number=%x WHERE post_id=%x", 
 				postRating.getNr_of_likes(),
 				postRating.getNr_of_dislikes(),
 				postRating.getPost_id()
 				);
-		runQuery(sql);
+		DB.runQuery(sql);
 	}
 }
